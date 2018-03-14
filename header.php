e75357bdabf83a2be70b06ca3edcec6aba58ed95
Denys <denv928@gmail.com>
2018-03-15 00:05:35 +0200

    Дрібні оновлення

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="/">Криптомания</a>
+        <a class="navbar-brand" href="/"><img class="img-fluid" width="300" src="/images/logo.png" alt="logo"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
